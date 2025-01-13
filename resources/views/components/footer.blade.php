3df65ad1ce1141a99bcd7a59747e9614b3fb17b6
sr-calcetines <pgavilansanc@gmail.com>
2025-01-13 15:46:14 +0100

    feat: added views for vehicles and tolls

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+    <p>&copy; 2025 Tolls and Vehicles Kata. All Rights Reserved.</p>
+    <div class="footer-anchor">
+      <a href="https://github.com/sr-calcetines"
+         <div> <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github Logo"></div>   
+      </a>
+    </div>
+</footer>
